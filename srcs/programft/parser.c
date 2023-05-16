196a0836512bfb6d92e789e8b189c554154220db
Andrea <aperrone@e3r11p3.42roma.it>
2023-05-16 20:28:10 +0200

    :)

--- srcs/programft/parser.c
+++ srcs/programft/parser.c
@@ -6,105 +6,11 @@
 /*   By: aperrone <aperrone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 04:48:42 by aperrone          #+#    #+#             */
-/*   Updated: 2023/05/16 17:44:46 by aperrone         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:56:45 by aperrone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/program.h"
-
-void	build_to_compare(char *temp, char *box)
-{
-	temp[0] = box[0];
-	temp[1] = box[1];
-	temp[2] = '\0';
-}
-
-int	box_compare(int *i, t_node *p_checks, char *temp)
-{
-	if (!ft_strcmp(temp, p_checks->box))
-	{
-		if (p_checks->status == 0)
-		{
-			p_checks->status = 1;
-			(*i)++;
-		}
-		else
-			return (0);
-	}
-	return (1);
-}
-
-int	presence_validator(t_node *info, t_fetch *fetched)
-{
-	t_node	*p_checks;
-	int		checks_len;
-	char	temp[3];
-	int		i;
-
-	fetched->p_t = info;
-	fetched->len = 6;
-	p_checks = fetched->checks;
-	i = 0;
-	while (fetched->p_t && fetched->len--)
-	{
-		build_to_compare(temp, fetched->p_t->box);
-		checks_len = 6;
-		while (p_checks && checks_len--)
-		{
-			if (box_compare(&i, p_checks, temp))
-				p_checks = p_checks->next;
-			else
-				return (0);
-		}
-		fetched->p_t = fetched->p_t->next;
-	}
-	return (i);
-}
-
-char	*line_reparator(char *box)
-{
-	int		i;
-	int		flag;
-	char	*out;
-
-	i = 0;
-	flag = 0;
-	out = malloc(1);
-	out[0] = 0;
-	while (box[i])
-	{
-		if (ft_isspace(box[i]) && flag == 0)
-		{
-			flag = 1;
-			ft_cpychar(box[i], &out);
-		}
-		else if (ft_isspace(box[i]) && flag == 1)
-		{
-			i++;
-			continue ;
-		}
-		else
-			ft_cpychar(box[i], &out);
-		i++;
-	}
-	return (out);
-}
-
-void	info_adjust(t_node **info, t_fetch *fetched)
-{
-	char	*temp;
-
-	fetched->p_t = *info;
-	fetched->len = 6;
-	while (fetched->p_t && fetched->len--)
-	{
-		temp = line_reparator(fetched->p_t->box);
-		free(fetched->p_t->box);
-		fetched->p_t->box = ft_strdup(temp);
-		free(temp);
-		fetched->p_t = fetched->p_t->next;
-	}
-}
 
 void		info_checks(t_node *info, t_fetch *fetched)
 {
@@ -153,22 +59,14 @@ int	map_validator(t_fetch **fetched)
 	{
 		i = -1;
 		while ((*fetched)->p_t->box[++i])
-		{
-			if ((*fetched)->p_t->box[i] != '1'
-				&& (*fetched)->p_t->box[i] != '0'
-				&& (*fetched)->p_t->box[i] != ' '
-				&& (*fetched)->p_t->box[i] != 'N'
-				&& (*fetched)->p_t->box[i] != 'S'
-				&& (*fetched)->p_t->box[i] != 'E'
-				&& (*fetched)->p_t->box[i] != 'W')
+			if (!valid_char((*fetched)->p_t->box[i]))
 				return (0);
-		}
 		(*fetched)->p_t = (*fetched)->p_t->next;
 	}
 	return (1);
 }
 
-int	build_information(int fd, t_node **info, t_fetch **fetched)
+int	build_information(int fd, t_node **info, t_cub **cub)
 {
 	char	*line;
 	int		i;
@@ -180,20 +78,20 @@ int	build_information(int fd, t_node **info, t_fetch **fetched)
 		if (ft_strcmp(line, "\n"))
 		{
 			if (i < 6)
-				add_t(info, new_(ft_strtrim(line, " '\t''\n'"), ++i));
+				add_t(info, new_(ft_strtrim(line, " \t\n"), ++i));
 			else
 			{
-				add_t(&(*fetched)->map, new_(ft_strtrim(line, " '\t''\n'"), ++i));
-				if ((*fetched)->len < ft_strlen(line))
-					(*fetched)->len = ft_strlen(line) - 1;
+				add_t(&(*cub)->fetched->map, new_(ft_strtrim(line, " \t\n"), ++i));
+				if ((*cub)->fetched->len < ft_strlen(line))
+					(*cub)->fetched->len = ft_strlen(line) - 1;
 			}
 		}
 		free(line);
 		line = get_next_line(fd);
 	}
-	relister(&(*fetched)->map);
-	if (map_validator(fetched))
-		if (map_normalizer(fetched))
+	relister(&(*cub)->fetched->map);
+	if (map_validator(&(*cub)->fetched))
+		if (map_normalizer(&(*cub)->fetched))
 			return (1);
 	return (0);
 }
@@ -205,7 +103,7 @@ int	parser(int fd, t_cub *cub)
 	info = NULL;
 	if (fd > 0)
 	{
-		if (build_information(fd, &info, &cub->fetched))
+		if (build_information(fd, &info, &cub))
 			info_checks(info, cub->fetched);
 		else
 			return (0);
